75444533a3f161df09e868a46a0c627ad2c89e49
Mohammed Alkhuzayyim <m.alkhuzayyim@gmail.com>
2019-08-05 07:31:52 +0300

    done until workshop topics

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,9 +1,70 @@
 <template>
   <v-row no-gutters class="primary">
-    <v-col cols="12" class="text-center mt-2">
-      <v-card-text class="white--text">
-        &copy;2019 — <strong>Joy Forum - Saudi Arabia</strong>
-      </v-card-text>
+    <v-col cols="12">
+      <v-row no-gutters class="mx-auto py-3" style="max-width: 1280px;">
+        <v-col cols="12" sm="3">
+          <v-card-title class="white--text font-weight-medium body-2">
+            EXHIBITION
+          </v-card-title>
+          <v-card-text class="white--text caption">
+            Exhibit Now <br />
+            Expo Details
+          </v-card-text>
+          <v-img class="ma-6" contain src="/images/footer-logo.png"></v-img>
+        </v-col>
+        <v-col cols="12" sm="3">
+          <v-card-title class="white--text font-weight-medium body-2">
+            REGISTRATION
+          </v-card-title>
+          <v-card-text class="white--text caption">
+            Attend the Exhibition <br />
+            Register to the Conference <br />
+            Attend the Workshops
+          </v-card-text>
+        </v-col>
+        <v-col cols="12" sm="3">
+          <v-card-title class="white--text font-weight-medium body-2">
+            LOCATION
+          </v-card-title>
+          <v-card-text class="white--text caption">
+            Riyadh, Kingdom of Saudi Arabia <br />
+            Ritz-Carlton Hotel
+          </v-card-text>
+        </v-col>
+        <v-col cols="12" sm="3">
+          <v-card-title class="white--text font-weight-medium body-2">
+            CONTACTS
+          </v-card-title>
+          <v-card-text class="white--text caption">
+            Expo Manager <br />
+            Ahmed Mufti <br />
+            +966 50 607 3695 <br />
+            Exhibit@joyforum.com
+          </v-card-text>
+          <div class="pl-2">
+            <v-card-title
+              class="pl-1 white--text pt-3 font-weight-bold subtitle-1"
+            >
+              JoyForumKSA
+            </v-card-title>
+            <v-btn fab depressed x-small color="white" class="mx-1">
+              <v-icon color="primary">mdi-linkedin</v-icon>
+            </v-btn>
+            <v-btn fab depressed x-small color="white" class="mx-1">
+              <v-icon color="primary">mdi-twitter</v-icon>
+            </v-btn>
+            <v-btn fab depressed x-small color="white" class="mx-1">
+              <v-icon color="primary">mdi-instagram</v-icon>
+            </v-btn>
+            <v-btn fab depressed x-small color="white" class="mx-1">
+              <v-icon color="primary">mdi-facebook</v-icon>
+            </v-btn>
+            <v-btn fab depressed x-small color="white" class="mx-1">
+              <v-icon color="primary">mdi-youtube</v-icon>
+            </v-btn>
+          </div>
+        </v-col>
+      </v-row>
     </v-col>
   </v-row>
 </template>
--- src/components/HomeComponents/WorkshopTopicsComponent.vue
+++ src/components/HomeComponents/WorkshopTopicsComponent.vue
@@ -12,7 +12,23 @@
     </v-col>
     <v-col cols="12">
       <v-row no-gutters class="pb-12">
-        <v-col cols="12"> </v-col>
+        <v-col cols="12">
+          <swiper :options="swiperOption">
+            <swiper-slide v-for="topic in workshopTopics" :key="topic.title">
+              <v-card tile flat color="#58a9ad">
+                <v-img contain :src="topic.imgSrc">
+                  <div
+                    class="white--text subtitle-2 text-center px-8"
+                    style="padding-top: 40%;"
+                  >
+                    {{ topic.title }}
+                  </div>
+                </v-img>
+              </v-card>
+            </swiper-slide>
+            <div class="swiper-pagination" slot="pagination"></div>
+          </swiper>
+        </v-col>
       </v-row>
     </v-col>
   </v-row>
@@ -24,42 +40,54 @@ export default {
     return {
       workshopTopics: [
         {
-          title: "Start your event management business"
+          title: "Start your Event Management Business",
+          imgSrc: "/images/workshops-start.jpg"
         },
         {
-          title: "Establishing your company"
+          title: "Establishing Your Company",
+          imgSrc: "/images/workshops-establishing.jpg"
         },
         {
-          title: "Valuation & Pitching Methods for new projects"
+          title: "Valuations & Pitching Methods",
+          imgSrc: "/images/workshops-valuation.jpg"
         },
         {
-          title: "Customer Journey Mapping"
+          title: "Customer Journey Mapping",
+          imgSrc: "/images/workshops-customerjourney.jpg"
         },
         {
-          title: "Content Curation"
+          title: "Content Curation",
+          imgSrc: "/images/workshops-contentcuration.jpg"
         },
         {
-          title: "Theme Park (getting started)"
+          title: "Theme Park (Getting Started)",
+          imgSrc: "/images/workshops-themepark.jpg"
         },
         {
-          title: "Crowd Management & Safety"
+          title: "Crowd Management & Safety",
+          imgSrc: "/images/workshops-crowdmanagement.jpg"
         },
         {
-          title: "Content Localization"
+          title: "Content Localization",
+          imgSrc: "/images/workshops-contentlocalization.jpg"
         },
         {
-          title: "Service & Experience Design"
+          title: "Service & Experience Design",
+          imgSrc: "/images/workshops-serviceexperience.jpg"
         },
         {
-          title: "Applying Design Thinking to Event Design"
+          title: "Applying Design Thinking to Event Design",
+          imgSrc: "/images/workshops-designthinking.jpg"
         }
       ],
       swiperOption: {
-        slidesPerView: 3,
-        spaceBetween: 30,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true
+        slidesPerView: 5,
+        spaceBetween: 3,
+        centeredSlides: true,
+        reverseDirection: true,
+        autoplay: {
+          delay: 2000,
+          disableOnInteraction: false
         }
       }
     };
